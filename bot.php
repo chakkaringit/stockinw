436a3564ca6132ed0db3a2df6f8bc671e63da11e
Chakkarin <sluvpy.kh@gmail.com>
2017-03-08 15:47:01 +0700

    Add SDK

--- bot.php
+++ bot.php
@@ -1,4 +1,7 @@
 <?php
+$path = __DIR__ . '/vendor/autoload.php';
+require_once $path;
+
 $channel_access_token = 'IJqpmoQ9nwIb9UztQnaaeJfY+B+R0sCw97kvQdM3D/DdMnfOO61HW7T96ex6UQNFs7M8UV1zdC4LRs+y8BXs2r09rVkGyNR6qbjl8n9zVS/nCyEu2gXauFhBTz4HBjj1QoLzeVGLsI89niQfGxp/ZQdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '6f5cf08d93f0988ceb5468a23c8fc789';
 
